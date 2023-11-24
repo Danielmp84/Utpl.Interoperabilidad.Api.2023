24fc86b78ce2501ac396e55d0abf15d3b01da1e9
DanielMP84 <122245978+Danielmp84@users.noreply.github.com>
2023-11-24 02:52:33 +0000

    Datos Cie10

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ class Paciente(BaseModel):
     city: str
     Cie10: str
     seguro: str
-    direccion: str
-    
-
+    direccion: str    
 
 # Lista para almacenar paciente (simulación de base de datos)
 paciente_db = []
